0bb088cd5d1dfc049c4d36c5f9606750f3558e35
Wendell <wendell@Wendells-Mac-mini.local>
2019-09-03 14:39:56 +0800

    quotes api

--- server.js
+++ server.js
@@ -1,13 +1,64 @@
-const http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
 const port = 3000;
+const app = express();
 
-const requestHandler = (request, response) => {
-  console.log(`New request to: ${request.url}`);
-  response.end("Hello, World!");
-}
+const quotes = [
+  {
+    id: 1,
+    quote: "The best is yet to come",
+    author: "Unknown",
+    year: 2000
+  },
+  {
+    id: 2,
+    quote: "This is a quote",
+    author: "First Last",
+    year: 1930
+  },
+  {
+    id: 3,
+    quote: "This is another quote",
+    author: "First2 Last2",
+    year: 1910
+  },
+  {
+    id: 4,
+    quote: "This is another quote 2",
+    author: "First2 Last2",
+    year: 1910
+  }
+];
 
-const server = http.createServer(requestHandler);
+app.listen(port, () => {
+  console.log("Now listening to port: ", port);
+});
 
-server.listen(port, ()=>{
-  console.log(`listening to port: ${port}`);
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello, Amigo");
+});
+
+app.get("/quotes", (req, res) => {
+  console.log("Get a list of all quotes as json");
+
+  if (req.query.year) {
+    console.log("Return a list of quotes from the year: " + req.query.year);
+    const filterYear = quotes.filter(q => q.year == req.query.year);
+
+    res.send(filterYear);
+  } else res.json(quotes);
+});
+
+app.get("/quotes/:id", (req, res) => {
+  console.log("Parameter id: ", req.params.id);
+
+  res.send(quotes.filter(q => q.id == req.params.id));
+});
+
+app.post("/quotes", (req, res) => {
+  console.log(`Insert a new quote: ${req.body.quote}`);
+
+  res.json(req.body);
+});
